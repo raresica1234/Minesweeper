9079692cfb67beaedcc978747f2b0fd13c793d17
raresica1234 <colorlord98@gmail.com>
2019-04-16 09:40:59 +0300

    Grid testcode

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,27 @@
 #include <SFML/Graphics.hpp>
 
 #include "Window.hpp"
+#include "Grid.hpp"
 
 int main()
 {
 	Window window("Minesweeper", 100, 100);
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+
+	Grid grid(32, 32, 2, 8, 8);
+
+	window.setSize(grid.getRequiredWidth(), grid.getRequiredHeight());
+
+	sf::RectangleShape fill(sf::Vector2f(100, 100));
+	fill.setFillColor(sf::Color::White);
 
 	while (window.isOpen()) {
 		window.update();
+		window.get()->draw(fill);
+		grid.draw(window);
 
 		window.display();
+
+		fill.setSize(sf::Vector2f(window.width, window.height));
 	}
 
 	return 0;
